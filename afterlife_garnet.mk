173628dda829e311a1b51e661ae39c7c8a6edef2
Drenzzz <naufalnazya@gmail.com>
2024-09-25 18:01:26 +0700

    garnet: go Afterlife

--- afterlife_garnet.mk
+++ afterlife_garnet.mk
@@ -9,15 +9,22 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 
 # Inherit some common Lineage stuff.
-$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
+$(call inherit-product, vendor/afterlife/config/common_full_phone.mk)
 $(call inherit-product, vendor/lineage-priv/keys/keys.mk)
 TARGET_BOOT_ANIMATION_RES := 1080
 TARGET_INCLUDE_PIXEL_CHARGER := true
 TARGET_FACE_UNLOCK_SUPPORTED := true
 
+# Afterlife Flags
+AFTERLIFE_MAINTAINER := Drenzzz
+TARGET_SUPPORTS_BLUR := true
+USE_PIXEL_CHARGING := true
+TARGET_SUPPORTS_TOUCHGESTURES := true
+TARGET_UDFPS_ANIMATIONS := true
+
 # Inherit from garnet device
 $(call inherit-product, device/xiaomi/garnet/device.mk)
-PRODUCT_NAME := lineage_garnet
+PRODUCT_NAME := afterlife_garnet
 PRODUCT_DEVICE := garnet
 PRODUCT_MANUFACTURER := Xiaomi
 PRODUCT_BRAND := Redmi
