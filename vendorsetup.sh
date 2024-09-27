dac274fd95baa0a64612fb2bfe7b15f6aa3aac87
Drenzzz <naufalnazya@gmail.com>
2024-09-27 20:39:35 +0700

    garnet: Initial lineage

---
+++ vendorsetup.sh
@@ -0,0 +1,14 @@
+echo 'Cloning stuffs to build for garnet'
+
+# Clone primary tree
+git clone https://github.com/Soverzion-HQ/vendor_xiaomi_garnet -b 15 vendor/xiaomi/garnet
+git clone https://github.com/Soverzion-HQ/device_xiaomi_garnet-prebuilt -b hikari device/xiaomi/garnet-prebuilt
+
+# Hardware Xiaomi
+rm -rf hardware/xiaomi
+git clone https://github.com/Soverzion-HQ/hardware_xiaomi -b lineage-22 hardware/xiaomi
+git clone https://gitlab.com/Soverzion-HQ/vendor_xiaomi_garnet-miuicamera vendor/xiaomi/garnet-miuicamera
+rm -rf hardware/qcom-caf/sm8450/display
+git clone https://github.com/Soverzion-HQ/hardware_qcom-caf_sm8450_display -b 14 hardware/qcom-caf/sm8450/display
+
+echo 'Cloning completed. It's time to lunch. Miaw'
