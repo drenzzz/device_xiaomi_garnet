f517edc84d8d5e424a1a01851cd1699fec88a5ee
Drenzzz <naufalnazya@gmail.com>
2024-10-13 11:21:42 +0700

    garnet: Initial euclidOS

--- euclid_garnet.mk
+++ euclid_garnet.mk
@@ -9,15 +9,27 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 
 # Inherit some common Lineage stuff.
-$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
+$(call inherit-product, vendor/euclid/config/common_full_phone.mk)
 $(call inherit-product, vendor/lineage-priv/keys/keys.mk)
 TARGET_BOOT_ANIMATION_RES := 1080
 TARGET_INCLUDE_PIXEL_CHARGER := true
 TARGET_FACE_UNLOCK_SUPPORTED := true
 
+# Euclid flags
+EXTRA_UDFPS_ANIMATIONS := true
+TARGET_HAS_UDFPS := true
+TARGET_SUPPORTS_TOUCHGESTURES := true
+EUCLID_BUILD_TYPE := UNOFFICIAL
+EUCLID_MAINTAINER := Drenzzz.
+EUCLID_GAPPS := true
+TARGET_SUPPORTS_GOOGLE_RECORDER := true
+TARGET_INCLUDE_STOCK_ARCORE := true
+TARGET_INCLUDE_LIVE_WALLPAPERS := true
+TARGET_INCLUDE_PIXEL_LAUNCHER := false
+
 # Inherit from garnet device
 $(call inherit-product, device/xiaomi/garnet/device.mk)
-PRODUCT_NAME := lineage_garnet
+PRODUCT_NAME := euclid_garnet
 PRODUCT_DEVICE := garnet
 PRODUCT_MANUFACTURER := Xiaomi
 PRODUCT_BRAND := Redmi
