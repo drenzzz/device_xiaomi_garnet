935c53f6392e70280f2b08c48bc0e90e8c92c5ef
Drenzzz <116653610+drenzzz@users.noreply.github.com>
2024-11-10 17:02:28 +0700

    Update vendorsetup.sh

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -4,12 +4,11 @@ echo 'Cloning stuffs to build for garnet'
 git clone --depth=1 https://github.com/Soverzion-HQ/vendor_xiaomi_garnet -b 14 vendor/xiaomi/garnet
 git clone --depth=1 https://github.com/Soverzion-HQ/device_xiaomi_garnet-prebuilt -b sk-ksu device/xiaomi/garnet-prebuilt
 
-# Hardware Xiaomi
+# Hw Xiaomi
 rm -rf hardware/xiaomi
 git clone https://github.com/Soverzion-HQ/hardware_xiaomi -b lineage-21 hardware/xiaomi
 git clone https://github.com/Soverzion-HQ/vendor_xiaomi_garnet-miuicamera -b 14 vendor/xiaomi/garnet-miuicamera
 rm -rf hardware/qcom-caf/sm8450/display
 git clone https://github.com/Soverzion-HQ/hardware_qcom-caf_sm8450_display -b 14 hardware/qcom-caf/sm8450/display
-git clone https://github.com/Soverzion-HQ/vendor_lineage-priv vendor/lineage-priv
 
 echo 'Cloning completed. It's time to lunch. Miaw'
